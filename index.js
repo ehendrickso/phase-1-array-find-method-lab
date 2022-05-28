bb6b06940fdee2d5d8d1b8456f7b3bca7ebd4025
+ <erol.hendrickson@gmail.com>
2022-05-28 13:42:03 -0500

    commit tree 13156a0b2ce33853a1c537ea07dc3af0077e3f8f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record) {
+    //return record.find(result ==="W"));
+    let football =  record.find(element => element.result === "W")
+   console.log(football)
+    if (football) {
+       return football.year
+   }
+   else return football
+
+   }
+
+   
